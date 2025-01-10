013103ae4379f39f3a92cbd31bfd567814d3998c
Raj Pabari <52271154+rajpabari@users.noreply.github.com>
2025-01-09 18:38:26 -0800

    finish step 1 lab 1, quine generation

--- labs/1-trusting-trust/code/step1/quine-gen.c
+++ labs/1-trusting-trust/code/step1/quine-gen.c
@@ -1,9 +1,21 @@
-// convert the contents of stdin to their ASCII values (e.g., 
+// convert the contents of stdin to their ASCII values (e.g.,
 // '\n' = 10) and spit out the <prog> array used in Figure 1 in
 // Thompson's paper.
 #include <stdio.h>
+#include <string.h>
 
-int main(void) { 
-    // put your code here.
-	return 0;
+#define MAX_SIZE 1000000
+
+int main(void)
+{
+    char buffer[MAX_SIZE];
+    int bytes_read = fread(buffer, 1, MAX_SIZE, stdin);
+    fprintf(stdout, "char prog[] = {\n");
+    for (int i = 0; i < bytes_read; i++)
+    {
+        fprintf(stdout, "\t%d,%c", buffer[i], (i + 1) % 8 == 0 ? '\n' : ' ');
+    }
+    printf("0 };\n");
+    printf("%s", buffer);
+    return 0;
 }
