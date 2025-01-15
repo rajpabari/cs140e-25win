58eb0faa1e8629c356a7b9c6a19152d82ad60a35
Raj Pabari <52271154+rajpabari@users.noreply.github.com>
2025-01-14 20:02:35 -0800

    finish lab 2 cs 140e

--- labs/2-gpio/code/4-loopback.c
+++ labs/2-gpio/code/4-loopback.c
@@ -4,22 +4,23 @@
 //   2. leave your LEDs connected to 20 and 21.
 //   3. if the test succeeds they should blink in unison.  either
 //      both on or both off.
-// 
+//
 // we do "true" embedded system debugging without any printk
 // available by using LEDs to show what the system is doing:
 //  iterate, alternating the value of <v> \in {0,1}
 //  1. set LED on 20 to current value of <v> (ground truth)
-//  2. set pin 18 (the loopback output) to <v>   
-//  3. read pin 19 (the loopback input) and set pin 21 
+//  2. set pin 18 (the loopback output) to <v>
+//  3. read pin 19 (the loopback input) and set pin 21
 //     to this value.
 //
 //  - if both LEDs match (both on at the same time, both off
-//    at the same time) you are correct.  
-//   - if not: first try the staff binary to make sure your 
+//    at the same time) you are correct.
+//   - if not: first try the staff binary to make sure your
 //     pi is wired correctly.
 #include "rpi.h"
 
-void notmain(void) {
+void notmain(void)
+{
     const int led0 = 20;
     const int led1 = 21;
     const int output = 9;
--- labs/2-gpio/code/gpio.c
+++ labs/2-gpio/code/gpio.c
@@ -1,8 +1,8 @@
 /*
- * Implement the following routines to set GPIO pins to input or 
+ * Implement the following routines to set GPIO pins to input or
  * output, and to read (input) and write (output) them.
- *  - DO NOT USE loads and stores directly: only use GET32 and 
- *    PUT32 to read and write memory.  
+ *  - DO NOT USE loads and stores directly: only use GET32 and
+ *    PUT32 to read and write memory.
  *  - DO USE the minimum number of such calls.
  * (Both of these matter for the next lab.)
  *
@@ -16,13 +16,14 @@
 //  - pointers use put32/get32.
 //  - integers: use PUT32/GET32.
 //  semantics are the same.
-enum {
-    GPIO_BASE = 0x20200000,
-    gpio_set0  = (GPIO_BASE + 0x1C),
-    gpio_clr0  = (GPIO_BASE + 0x28),
-    gpio_lev0  = (GPIO_BASE + 0x34)
+enum
+{
+  GPIO_BASE = 0x20200000, // base == fsel0
+  gpio_set0 = (GPIO_BASE + 0x1C),
+  gpio_clr0 = (GPIO_BASE + 0x28),
+  gpio_lev0 = (GPIO_BASE + 0x34)
 
-    // <you may need other values.>
+  // <you may need other values.>
 };
 
 //
@@ -33,36 +34,50 @@ enum {
 //
 // note: fsel0, fsel1, fsel2 are contiguous in memory, so you
 // can (and should) use array calculations!
-void gpio_set_output(unsigned pin) {
-    if(pin >= 32 && pin != 47)
-        return;
+void gpio_set_output(unsigned pin)
+{
+  if (pin >= 32 && pin != 47)
+    return;
 
   // implement this
   // use <gpio_fsel0>
+  unsigned address = GPIO_BASE + 0x00000004 * (pin / 10);
+  unsigned mask = 0x00000007 << 3 * (pin % 10);
+  unsigned bits = 0x00000001 << 3 * (pin % 10);
+  unsigned value = GET32(address);
+  value = (value & ~mask) | bits;
+  PUT32(address, value);
 }
 
 // set GPIO <pin> on.
-void gpio_set_on(unsigned pin) {
-    if(pin >= 32 && pin != 47)
-        return;
+void gpio_set_on(unsigned pin)
+{
+  if (pin >= 32 && pin != 47)
+    return;
+  unsigned address = gpio_set0 + 0x4 * (pin / 32);
+  PUT32(address, 0x00000001 << (pin % 32));
   // implement this
   // use <gpio_set0>
 }
 
 // set GPIO <pin> off
-void gpio_set_off(unsigned pin) {
-    if(pin >= 32 && pin != 47)
-        return;
+void gpio_set_off(unsigned pin)
+{
+  if (pin >= 32 && pin != 47)
+    return;
+  unsigned address = gpio_clr0 + 0x4 * (pin / 32);
+  PUT32(address, 0x00000001 << (pin % 32));
   // implement this
   // use <gpio_clr0>
 }
 
 // set <pin> to <v> (v \in {0,1})
-void gpio_write(unsigned pin, unsigned v) {
-    if(v)
-        gpio_set_on(pin);
-    else
-        gpio_set_off(pin);
+void gpio_write(unsigned pin, unsigned v)
+{
+  if (v)
+    gpio_set_on(pin);
+  else
+    gpio_set_off(pin);
 }
 
 //
@@ -70,15 +85,24 @@ void gpio_write(unsigned pin, unsigned v) {
 //
 
 // set <pin> to input.
-void gpio_set_input(unsigned pin) {
+void gpio_set_input(unsigned pin)
+{
   // implement.
+  unsigned address = GPIO_BASE + 0x00000004 * (pin / 10);
+  unsigned mask = 0x00000007 << 3 * (pin % 10);
+  unsigned value = GET32(address);
+  value = (value & ~mask);
+  PUT32(address, value);
 }
 
 // return the value of <pin>
-int gpio_read(unsigned pin) {
+int gpio_read(unsigned pin)
+{
   unsigned v = 0;
 
   // implement!
+  unsigned address = gpio_lev0 + (0x000000A * (pin / 32));
+  unsigned value = GET32(address);
 
-  return v;
+  return (value >> pin) & 1;
 }
